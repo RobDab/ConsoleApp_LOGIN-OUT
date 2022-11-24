37dfbf6d5026559a0fa4c4e10bdab7f22230feb9
RobDab <roberto.dabb@gmail.com>
2022-11-24 18:56:19 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp_LOGIN-OUT/Classes/User.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_LOGIN_OUT.Classes
+{
+    internal static class User
+    {
+        public static string Username;
+
+        public static string Password;
+
+        public static bool isLogged = false;
+
+        public static DateTime Log;
+
+        public static List<DateTime> AccessLog = new List<DateTime>();
+
+
+
+        //METHODS ------
+        public static void Login()
+        {
+            do
+            {
+                Console.WriteLine("Inserisci Username: _");
+                Username = Console.ReadLine();
+                if (Username == "")
+                {
+                    Console.WriteLine("Inserisci uno Username valida");
+                }
+            } while (Username == "");
+            do
+            {
+                string psw1;
+                string psw2;
+                Password = "";
+
+                Console.WriteLine("Inserisci Password _ ");
+                psw1 = Console.ReadLine();
+                Console.WriteLine("Conferma Password _ ");
+                psw2 = Console.ReadLine();
+                
+                if(psw1 == psw2 && psw1!= "")
+                {
+                    Password = psw1;
+                    isLogged = true;
+                    Log = DateTime.Now;
+                    AccessLog.Add(Log);
+                }
+                else
+                {
+                    Console.WriteLine("Inserisci password uguali NON vuote! clicca 'INVIO'");
+                    Console.ReadLine();
+                }
+            } while (Password == "");
+        }
+
+        public static void Logout()
+        {
+            
+            if (isLogged)
+            {
+                Console.WriteLine("***** See you soon! :D *****");
+                Username = null;
+                Password = null;
+                isLogged = false;
+                
+            }
+            else
+            {
+                Console.WriteLine("There is no loggged User. LogIn to start! Click on 'Enter'");
+                
+                
+            }
+            
+            
+        }
+
+        public static void DisplayLog()
+        {
+            Console.WriteLine("\n<><><><><><><><><><><>");
+            Console.WriteLine($"Current Log: {Log.ToLocalTime().ToString()}");
+            Console.WriteLine($"User: {Username}");
+            Console.WriteLine("\n<><><><><><><><><><><>");
+        }
+
+        public static void DisplayAccessLog()
+        {
+            Console.WriteLine("\n<><><><><><><><><><><>");
+            foreach (DateTime log in AccessLog)
+            {
+                Console.WriteLine($"\n< {log.ToLocalTime().ToString()} >");
+            }
+            Console.WriteLine("\n<><><><><><><><><><><>");
+        }
+    }
+}
---
+++ ConsoleApp_LOGIN-OUT/Program.cs
@@ -0,0 +1,111 @@
+﻿using ConsoleApp_LOGIN_OUT.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_LOGIN_OUT
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("-* BENVENUTO *-");
+            DisplayMenu();
+
+
+        }
+
+
+        public static void DisplayMenu()
+        {
+            string Input;
+            Console.WriteLine("\nScegli un'operazione tra le seguenti:");
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Logout");
+            Console.WriteLine("3. Ora e Data Accesso");
+            Console.WriteLine("4. Access Log");
+            Console.WriteLine("q. *Quit session*");
+
+            do
+            {
+
+                Input = Console.ReadLine();
+                if (
+                    Input != "1"
+                && Input != "2"
+                && Input != "3"
+                && Input != "4"
+                && Input != "q"
+
+                    )
+                {
+                    Console.WriteLine("Inserisci una delle opzioni elencate e riprova.");
+                };
+            }while (
+                   Input != "1"
+                && Input != "2"
+                && Input != "3"
+                && Input != "4"
+                && Input != "q"
+                );
+
+
+
+            switch (Input)
+            {
+                case "1":
+                    if (!User.isLogged)
+                    {
+                        User.Login();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Already Logged-in!");
+                    }
+                    break;
+                case "2":
+                    User.Logout();
+
+                    break;
+
+                case "3":
+
+
+                    if (User.isLogged)
+                    {
+                        User.DisplayLog();
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no loggged User. LogIn to start! Click 'Enter'");
+
+                    }
+
+                    break;
+
+                case "4":
+                    // creare metodo in user per display access log
+                    User.DisplayAccessLog();
+
+                    break;
+                case "q":
+                    Console.WriteLine("\n*** Come back soon!! >_<'\n" +
+                        "'enter' to quit.");
+
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                    break;
+            }
+            
+            DisplayMenu();
+        }
+
+    }   
+}
